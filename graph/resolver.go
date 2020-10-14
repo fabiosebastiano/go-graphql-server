e802be482fb5074159dece3d5f5775852eb85f5d
Fabio Sebastiano <sebastiano.fabio@gmail.com>
2020-10-14 09:53:50 +0200

    implementato MUTATION e QUERY

---
+++ graph/resolver.go
@@ -0,0 +1,15 @@
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+//go:generate go run github.com/99designs/gqlgen
+
+import (
+	"github.com/fabiosebastiano/graphql-server/graph/model"
+)
+
+type Resolver struct {
+	videos []*model.Video
+}
